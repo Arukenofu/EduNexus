e3759b69e878a513faea8292e070d2e5b2c30fb8
Arukenofu <balkenov711@gmail.com>
2024-03-20 01:03:24 +0600

    initialize

--- components/IndividualLessons.vue
+++ components/IndividualLessons.vue
@@ -10,7 +10,13 @@
       <div class="teacher-image" />
 
       <div class="teacher-info">
+        <p class="teacher-name">
+          О. И. Димашева
 
+          <span>
+
+          </span>
+        </p>
       </div>
 
     </div>
@@ -30,9 +36,26 @@
     border-radius: 6px;
     border: 1px solid var(--bg-secondary);
     aspect-ratio: 1/0.7;
-    padding: 9px;
     transition: transform 0.3s cubic-bezier(0, 0, 0.5, 1);
     cursor: pointer;
+    display: flex;
+
+    .teacher-image {
+      height: 100%;
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+      background-color: var(--bg-secondary);
+      width: 45%;
+    }
+
+    .teacher-info {
+      padding: 24px 12px;
+
+      .teacher-name {
+        font-weight: 700;
+      }
+
+    }
 
   }
 }
