97f4aabc2234dc9aede6948ab99d9c676007e384
Arukenofu <balkenov711@gmail.com>
2024-05-13 11:49:19 +0600

    added styles for <code> and <ul><li> tags

--- pages/learn/[course]/[module]/lecture/[id].vue
+++ pages/learn/[course]/[module]/lecture/[id].vue
@@ -38,33 +38,53 @@ const {data: lecture, error} = await useAPI<Lecture>(`/learning/${course}/${id}`
   :deep() {
     b {
         font-weight: 700;
-      }
+    }
 
     h1 {
         font-size: 2.75em;
         font-weight: 800;
         line-height: 1.2;
         margin-bottom: 24px;
-      }
+    }
 
     h2 {
         font-size: 1.9em;
         font-weight: 700;
-      }
+    }
 
     h3 {
         font-size: 1.5em;
-      }
+    }
 
     p {
         font-size: 1.1em;
         line-height: 1.4;
-      }
+    }
 
     img {
         width: 100%;
         border-radius: 8px;
+    }
+
+    ul {
+
+      li {
+        margin-left: 15px;
       }
+    }
+
+    pre {
+      background-color: #18181a;
+      padding: 12px 21px;
+      border-radius: 0.3em;
+      border: 1px solid var(--border);
+      user-select: none;
+
+      code {
+        white-space: inherit;
+        font-weight: 700;
+      }
+    }
   }
 }
 
